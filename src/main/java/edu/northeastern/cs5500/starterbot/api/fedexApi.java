2300c2e99d4fb512beea4d4db1e561eb04e0bb9e
Kelvin Lim <25336322+thekelvinlime@users.noreply.github.com>
2023-03-17 16:44:11 +0000

    Adding track.json, authorization.json, and some fedexAPI methods in api folder

---
+++ src/main/java/edu/northeastern/cs5500/starterbot/api/fedexApi.java
@@ -0,0 +1,53 @@
+package edu.northeastern.cs5500.starterbot.api;
+
+public class fedexApi {
+    public void authentication() {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        // 'input' refers to JSON Payload
+        RequestBody body = RequestBody.create(mediaType, input);
+        Request request = new Request.Builder()
+            .url("https://apis-sandbox.fedex.com/oauth/token")
+            .post(body)
+            .addHeader("Content-Type", "application/x-www-form-urlencoded")
+            .build();
+                    
+        Response response = client.newCall(request).execute();
+    }
+
+    public void track() {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        // 'input' refers to JSON Payload
+        RequestBody body = RequestBody.create(mediaType, input);
+        Request request = new Request.Builder()
+            .url("https://apis-sandbox.fedex.com/track/v1/referencenumbers")
+            .post(body)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("X-locale", "en_US")
+            .addHeader("Authorization", "Bearer ")
+            .build();
+                    
+        Response response = client.newCall(request).execute();
+    }
+
+    public void notification() {
+        OkHttpClient client = new OkHttpClient();
+
+        MediaType mediaType = MediaType.parse("application/json");
+        // 'input' refers to JSON Payload
+        RequestBody body = RequestBody.create(mediaType, input);
+        Request request = new Request.Builder()
+            .url("https://apis-sandbox.fedex.com/track/v1/notifications")
+            .post(body)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("X-locale", "en_US")
+            .addHeader("Authorization", "Bearer ")
+            .build();
+                    
+        Response response = client.newCall(request).execute();
+    }
+
+}
