d89a2a9b053665491bffb3a2ba1fa86c727d1e49
Cece <45926850+SIXIN-LI@users.noreply.github.com>
2023-04-01 05:41:48 +0000

    temp save

--- src/main/java/edu/northeastern/cs5500/starterbot/controller/PackageController.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/controller/PackageController.java
@@ -2,14 +2,22 @@ package edu.northeastern.cs5500.starterbot.controller;
 
 import edu.northeastern.cs5500.starterbot.model.Package;
 import edu.northeastern.cs5500.starterbot.repository.GenericRepository;
+import edu.northeastern.cs5500.starterbot.service.TrackPackageService;
+
 import javax.inject.Inject;
 
 public class PackageController {
 
     GenericRepository<Package> packageRepository; // data access object
+    TrackPackageService trackPackageService;
 
     @Inject
     public PackageController(GenericRepository<Package> packageRepository) {
         this.packageRepository = packageRepository;
+        this.trackPackageService = TrackPackageService.getTrackPackageService();
+    }
+
+    public String createPackage() {
+        return trackPackageService.createPackageTracking(null);
     }
 }
--- src/main/java/edu/northeastern/cs5500/starterbot/service/TrackPackageService.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/service/TrackPackageService.java
@@ -1,39 +1,174 @@
+package edu.northeastern.cs5500.starterbot.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 
 @Singleton
 @Slf4j
-public class MongoDBService implements Service {
+public class TrackPackageService implements Service {
+
+    private final String URL = "https://www.kd100.com/api/v1/tracking/create";
+    private final String API_KEY = "PoceRRIqNjYd1883";
+    private final String SECRET = "1db9a0ce49d341f1b932b12e0fccdfd6";
+    private final int CONNECT_TIMEOUT = 1000;
+    private final int READ_TIMEOUT = 5000;
+    private final String SUCCESS = "success";
 
     /**
-     * Get updates along the way, not instantly displayed
+     * Get updates along the way Delivery status is not instantly displayed
+     *
+     * @return if a package is successfully created
+     */
+    public String createPackageTracking(Package package1) {
+        // mock data
+        String carrier_id = "ups";
+        String tracking_number = "1Z9A170W0337231977";
+        String webhook_url = "https://www.kd100.com/webhook_url";
+
+        String result = getData(carrier_id, tracking_number, webhook_url);
+
+        // read the result and handle errors
+        return readResponse(result);
+    }
+
+    private String getData(String carrier_id, String tracking_number, String webhook_url) {
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("carrier_id", carrier_id);
+        jsonObject.put("tracking_number", tracking_number);
+        jsonObject.put("webhook_url", webhook_url);
+        String param = jsonObject.toString();
+
+        String signature = MD5Utils.encode(param + API_KEY + SECRET);
+        return this.post(param, signature);
+    }
+
+    private String post(String param, String signature) {
+        StringBuffer response = new StringBuffer("");
+
+        byte[] data = param.getBytes();
+
+        BufferedReader reader = null;
+        OutputStream out = null;
+        try {
+
+            URL url = new URL(URL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(CONNECT_TIMEOUT);
+            conn.setReadTimeout(READ_TIMEOUT);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Content-Length", String.valueOf(data.length));
+            conn.setRequestProperty("API-Key", API_KEY);
+            conn.setRequestProperty("signature", signature);
+            conn.setDoOutput(true);
+
+            out = conn.getOutputStream();
+            out.write(data);
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != out) {
+                    out.flush();
+                    out.close();
+                }
+                if (null != reader) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return response.toString();
+    }
+
+    /** Invoked when displaying list of packages Calling real-time tracking api */
+    public void getPackageCurrentStatus() {}
+
+    /** Updates about packages will be posted via webhook */
+    private void handlePackageUpdates() {}
+
+    /**
+     * When creating the tracking, if the response code is 407, message is "The tracking_number is
+     * invalid", return false
+     *
      * @return
      */
-    public boolean createPackageTracking() {
-        return true;
+    public String readResponse(String result) {
+        JSONObject json = new JSONObject(result);
+
+        int code = json.getInt("code");
+        String message = json.getString("message");
+
+        if (code != 200) return message;
+
+        return SUCCESS;
     }
 
-    /**
-     * Invoked when displaying list of packages
-     */
-    public void getPackageCurrentStatus() {
+    @Override
+    public void register() {
+        log.info("TrackPackageService > register");
+    }
+}
 
+class MD5Utils {
+    private static MessageDigest mdigest = null;
+    private static char digits[] = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+    };
+
+    private static MessageDigest getMdInst() {
+        if (null == mdigest) {
+            try {
+                mdigest = MessageDigest.getInstance("MD5");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+        return mdigest;
     }
 
-    /**
-     * Updates about packages will be posted via webhook
-     */
-    private void handlePackageUpdates() {
+    public static String encode(String s) {
+        if (null == s) {
+            return "";
+        }
 
+        try {
+            byte[] bytes = s.getBytes();
+            getMdInst().update(bytes);
+            byte[] md = getMdInst().digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = digits[byte0 >>> 4 & 0xf];
+                str[k++] = digits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
-
-    /**
-     * When creating the tracking, if the response code is 407,
-     * message is "The tracking_number is invalid", return false
-     * 
-     * @return
-     */
-    public boolean verifyTrackingNumber() {
-        return true;
-    }
-    
 }
