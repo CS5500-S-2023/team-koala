5b04adbd3d94378c28b29499aea17d982401194e
Cece <45926850+SIXIN-LI@users.noreply.github.com>
2023-04-01 06:47:18 +0000

    fix minor issues and add test for createPackage in controller

--- src/main/java/edu/northeastern/cs5500/starterbot/controller/PackageController.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/controller/PackageController.java
@@ -3,11 +3,12 @@ package edu.northeastern.cs5500.starterbot.controller;
 import edu.northeastern.cs5500.starterbot.model.Package;
 import edu.northeastern.cs5500.starterbot.repository.GenericRepository;
 import javax.inject.Inject;
+import org.bson.types.ObjectId;
 
 public class PackageController {
 
     GenericRepository<Package> packageRepository; // data access object
-    public final String SUCCESS_STATUS = "success";
+    public final String SUCCESS = "success";
 
     @Inject
     public PackageController(GenericRepository<Package> packageRepository) {
@@ -20,9 +21,13 @@ public class PackageController {
         // create tracking item
 
         // write to database
-        // assumed success, otherwise will need to modify repository functions
+        // assumed insertion success, otherwise will need to modify repository functions
         packageRepository.add(package1);
 
-        return SUCCESS_STATUS;
+        return SUCCESS;
+    }
+
+    public Package getPackage(ObjectId id) {
+        return this.packageRepository.get(id);
     }
 }
--- src/main/java/edu/northeastern/cs5500/starterbot/model/Package.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/Package.java
@@ -1,12 +1,14 @@
 package edu.northeastern.cs5500.starterbot.model;
 
 import javax.annotation.Nonnull;
+import lombok.AllArgsConstructor; // only for test purpose
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Package implements Model {
 
     ObjectId id;
