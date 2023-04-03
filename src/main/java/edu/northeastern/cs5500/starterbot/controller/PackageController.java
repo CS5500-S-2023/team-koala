d77da3a6ba2dec5aba4ff9cc8a81f55dd6986ded
Kelvin Lim <25336322+thekelvinlime@users.noreply.github.com>
2023-04-03 02:11:40 +0000

    Adding ArrayList import to DisplayPackagesCommand but need a review

--- src/main/java/edu/northeastern/cs5500/starterbot/controller/PackageController.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/controller/PackageController.java
@@ -2,6 +2,7 @@ package edu.northeastern.cs5500.starterbot.controller;
 
 import edu.northeastern.cs5500.starterbot.model.Package;
 import edu.northeastern.cs5500.starterbot.repository.GenericRepository;
+import java.util.ArrayList;
 import javax.inject.Inject;
 import org.bson.types.ObjectId;
 
