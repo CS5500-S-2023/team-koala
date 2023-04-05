1685bdee03007812ce25f6e78d6a7ace656de6d0
Cece <45926850+SIXIN-LI@users.noreply.github.com>
2023-04-05 22:59:48 +0000

    update package info in database  after getting updates

--- src/main/java/edu/northeastern/cs5500/starterbot/controller/PackageController.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/controller/PackageController.java
@@ -2,14 +2,28 @@ package edu.northeastern.cs5500.starterbot.controller;
 
 import edu.northeastern.cs5500.starterbot.model.Package;
 import edu.northeastern.cs5500.starterbot.repository.GenericRepository;
+import edu.northeastern.cs5500.starterbot.service.TrackPackageService;
 import javax.inject.Inject;
 
 public class PackageController {
 
     GenericRepository<Package> packageRepository; // data access object
+    TrackPackageService trackPackageService;
 
     @Inject
-    public PackageController(GenericRepository<Package> packageRepository) {
+    public PackageController(
+            GenericRepository<Package> packageRepository, TrackPackageService trackPackageService) {
         this.packageRepository = packageRepository;
+        this.trackPackageService = trackPackageService;
+    }
+
+    /**
+     * The updated status will be written in the passed-in package object If the status and
+     * statusTime is null, then it means no status is available yet.
+     *
+     * @param package1
+     */
+    public void getPackageLatestStatus(Package package1) {
+        trackPackageService.getPackageLatestStatus(package1);
     }
 }
--- src/test/java/edu/northeastern/cs5500/starterbot/Service/TrackPackageServiceTest.java
+++ src/test/java/edu/northeastern/cs5500/starterbot/Service/TrackPackageServiceTest.java
@@ -3,6 +3,7 @@ package edu.northeastern.cs5500.starterbot.Service;
 import static org.junit.Assert.*;
 
 import edu.northeastern.cs5500.starterbot.model.Package;
+import edu.northeastern.cs5500.starterbot.repository.InMemoryRepository;
 import edu.northeastern.cs5500.starterbot.service.TrackPackageService;
 import org.junit.jupiter.api.Test;
 
@@ -11,7 +12,7 @@ public class TrackPackageServiceTest {
 
     TrackPackageServiceTest() {
         // even though this is a singleton, it can still be tested like a normal class
-        this.trackPackageService = new TrackPackageService();
+        this.trackPackageService = new TrackPackageService(new InMemoryRepository<>());
     }
 
     @Test
@@ -20,7 +21,7 @@ public class TrackPackageServiceTest {
         package1.setCarrierId("ups");
         package1.setTrackingNumber("1Z9A170W0337231977");
 
-        package1 = trackPackageService.getPackageLatestStatus(package1);
+        trackPackageService.getPackageLatestStatus(package1);
         assertEquals("[SEATTLE, WA, US]DELIVERED", package1.getStatus());
     }
 }
