26aedda1c4cf15c4173cc649e96d372c410dd7e0
Cece <45926850+SIXIN-LI@users.noreply.github.com>
2023-04-05 22:43:25 +0000

    get delivery status via api

--- src/main/java/edu/northeastern/cs5500/starterbot/model/Package.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/Package.java
@@ -1,19 +1,23 @@
 package edu.northeastern.cs5500.starterbot.model;
 
+import java.time.LocalDateTime;
+
 import javax.annotation.Nonnull;
 import lombok.AllArgsConstructor; // only for test purpose
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
 
+import com.mongodb.lang.Nullable;
+
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Package implements Model {
 
     ObjectId id;
-    String name;
+    @Nullable final String name;
     @Nonnull String trackingNumber;
     @Nonnull String carrierId;
-    @Nonnull String userId;
+    @Nullable String status;
+    @Nullable LocalDateTime statusTime;
+    
 }
--- src/main/java/edu/northeastern/cs5500/starterbot/service/TrackPackageService.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/service/TrackPackageService.java
@@ -1,39 +1,197 @@
+package edu.northeastern.cs5500.starterbot.service;
 
+import edu.northeastern.cs5500.starterbot.model.Package;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 @Singleton
 @Slf4j
-public class MongoDBService implements Service {
+public class TrackPackageService implements Service {
+
+    private final String REALTIME_URL = "https://www.kd100.com/api/v1/tracking/realtime";
+    // will change to environmental variable later like BOT_TOKEN
+    private final String API_KEY = "PoceRRIqNjYd1883";
+    private final String SECRET = "1db9a0ce49d341f1b932b12e0fccdfd6";
+    private final int CONNECT_TIMEOUT = 1000;
+    private final int READ_TIMEOUT = 5000;
+    public final String SUCCESS = "success";
+
+    @Inject
+    public TrackPackageService() {}
 
     /**
-     * Get updates along the way, not instantly displayed
+     * Call real-time tracking api to get the latest status Invoked when displaying list of packages
+     *
+     * @return package1 - If there no delivery updates, status and statusTime in Package will be
+     *     null - Otherwise, they are not null
+     */
+    public Package getPackageLatestStatus(Package package1) {
+        String carrier_id = package1.getCarrierId();
+        String tracking_number = package1.getTrackingNumber();
+
+        String result = getData(REALTIME_URL, carrier_id, tracking_number, null);
+
+        // read the delivery updates
+        return readDeliveryResponse(result, package1);
+    }
+
+    /**
+     * Parse the response from KeyDelivery to get their delivery latest status, time and description
+     * Then, update package info in database
+     *
+     * @param result
      * @return
      */
-    public boolean createPackageTracking() {
-        return true;
+    private Package readDeliveryResponse(String result, Package package1) {
+        JSONObject json = new JSONObject(result);
+        JSONObject data = json.getJSONObject("data");
+        String carrier = data.getString("carrier_id");
+        String trackingNumber = data.getString("tracking_number");
+
+        JSONArray deliveryStatuses = data.getJSONArray("items");
+        JSONObject latestStatus =
+                deliveryStatuses.length() == 0 ? null : (JSONObject) deliveryStatuses.get(0);
+
+        String status = latestStatus.getString("context");
+
+        String statusTime = latestStatus.getString("time");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime time = LocalDateTime.parse(statusTime, formatter);
+
+        package1.setStatus(status);
+        package1.setStatusTime(time);
+
+        return package1;
     }
 
     /**
-     * Invoked when displaying list of packages
+     * Construct JSON strings and send post requests to a KeyDelivery API
+     *
+     * @param carrier_id
+     * @param tracking_number
+     * @param webhook_url
+     * @return response from KeyDelivery
      */
-    public void getPackageCurrentStatus() {
+    private String getData(
+            String url, String carrier_id, String tracking_number, String webhook_url) {
 
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("carrier_id", carrier_id);
+        jsonObject.put("tracking_number", tracking_number);
+        jsonObject.put("webhook_url", webhook_url);
+        String param = jsonObject.toString();
+
+        String signature = MD5Utils.encode(param + API_KEY + SECRET);
+        return this.post(param, signature, url);
     }
 
-    /**
-     * Updates about packages will be posted via webhook
-     */
-    private void handlePackageUpdates() {
+    private String post(String param, String signature, String urlTarget) {
+        StringBuffer response = new StringBuffer("");
 
+        byte[] data = param.getBytes();
+
+        BufferedReader reader = null;
+        OutputStream out = null;
+        try {
+
+            URL url = new URL(urlTarget);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setConnectTimeout(CONNECT_TIMEOUT);
+            conn.setReadTimeout(READ_TIMEOUT);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("accept", "*/*");
+            conn.setRequestProperty("connection", "Keep-Alive");
+            conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Content-Length", String.valueOf(data.length));
+            conn.setRequestProperty("API-Key", API_KEY);
+            conn.setRequestProperty("signature", signature);
+            conn.setDoOutput(true);
+
+            out = conn.getOutputStream();
+            out.write(data);
+
+            reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != out) {
+                    out.flush();
+                    out.close();
+                }
+                if (null != reader) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return response.toString();
     }
 
-    /**
-     * When creating the tracking, if the response code is 407,
-     * message is "The tracking_number is invalid", return false
-     * 
-     * @return
-     */
-    public boolean verifyTrackingNumber() {
-        return true;
+    @Override
+    public void register() {
+        log.info("TrackPackageService > register");
     }
-    
+}
+
+class MD5Utils {
+    private static MessageDigest mdigest = null;
+    private static char digits[] = {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+    };
+
+    private static MessageDigest getMdInst() {
+        if (null == mdigest) {
+            try {
+                mdigest = MessageDigest.getInstance("MD5");
+            } catch (NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+        return mdigest;
+    }
+
+    public static String encode(String s) {
+        if (null == s) {
+            return "";
+        }
+
+        try {
+            byte[] bytes = s.getBytes();
+            getMdInst().update(bytes);
+            byte[] md = getMdInst().digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = digits[byte0 >>> 4 & 0xf];
+                str[k++] = digits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
