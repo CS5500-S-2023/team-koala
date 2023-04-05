592054b29bf0892a484f3c857659160b2b3502b8
Cece <45926850+SIXIN-LI@users.noreply.github.com>
2023-04-05 22:43:25 +0000

    get delivery status via api

--- src/main/java/edu/northeastern/cs5500/starterbot/model/Package.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/model/Package.java
@@ -1,6 +1,5 @@
 package edu.northeastern.cs5500.starterbot.model;
 
-import javax.annotation.Nonnull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
@@ -11,12 +10,9 @@ public class Package implements Model {
 
     ObjectId id;
     String name;
-    @Nonnull String trackingNumber;
-    @Nonnull String carrierId;
-
-    public void reset() {
-        this.name = null;
-        this.trackingNumber = null;
-        this.carrierId = null;
-    }
+    String trackingNumber;
+    String carrierId;
+    String status;
+    LocalDateTime statusTime;
+    
 }
