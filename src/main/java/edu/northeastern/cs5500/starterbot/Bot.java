00f393ae92def6175698c01af405d04e24d509ea
Cece <45926850+SIXIN-LI@users.noreply.github.com>
2023-04-17 07:54:59 +0000

    Set the task to execute at the noon of each day

--- src/main/java/edu/northeastern/cs5500/starterbot/Bot.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/Bot.java
@@ -7,12 +7,21 @@ import edu.northeastern.cs5500.starterbot.repository.RepositoryModule;
 import edu.northeastern.cs5500.starterbot.service.ServiceModule;
 import edu.northeastern.cs5500.starterbot.service.GetPackageStatusTask;
 import edu.northeastern.cs5500.starterbot.service.TrackPackageService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Date;
+import java.util.EnumSet;
+import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 @Component(modules = {CommandModule.class, RepositoryModule.class, ServiceModule.class})
 @Singleton
@@ -45,10 +54,26 @@ public class Bot {
 
     /**
      * Only notify users when there are updates for the package
+     * 
+     * @param - jda is needed to send messages to discord user
      */
     public void getPackageStatusDaily(JDA jda) {
         Timer timer = new Timer();
         TimerTask task = new GetPackageStatusTask(jda, trackPackageService);
-        timer.schedule(task, 2000, 100000);
+
+        // The task is expected to run daily at noon of the day
+        String startTime = "2023-04-17 12:00"; // By default, it is 24-hour clock
+        String pattern = "yyyy-MM-dd hh:mm"; 
+        SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+        Date startDate;
+        try {
+            startDate = formatter.parse(startTime);
+        } catch (ParseException e) {
+            // This should never be reached as the input is valid and constant
+            return;
+        }
+        long intervalInMilliseconds = TimeUnit.DAYS.toMillis(1);
+
+        timer.schedule(task, startDate, intervalInMilliseconds);
     }
 }
--- src/main/java/edu/northeastern/cs5500/starterbot/service/GetPackageStatusTask.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/service/GetPackageStatusTask.java
@@ -1,12 +1,19 @@
 package edu.northeastern.cs5500.starterbot.service;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.TimerTask;
+import java.util.Map.Entry;
+
 import net.dv8tion.jda.api.JDA;
 import lombok.extern.slf4j.Slf4j;
 import edu.northeastern.cs5500.starterbot.model.Package;
 
+/*
+ * This is a task which is scheduled to send the updated status of all packages
+ */
 @Slf4j
 public class GetPackageStatusTask extends TimerTask {
     private JDA jda;
@@ -17,14 +24,23 @@ public class GetPackageStatusTask extends TimerTask {
         this.trackPackageService = trackPackageService;
     }
 
+    /**
+     * Send the updated status of all packages in our database to users
+     * 
+     * 1. Get all packages from the MongoDB database
+     * 2. Get the latest status of packages via third-party API
+     * 3. If the status of a package doesn't have updates, no update will be sent to the user.
+     * 4. Send a private message to users of packages about latest status
+     */
     @Override
     public void run() {
-        // retrieve all packages
+        // retrieve all packages from the database
         Collection<Package> allPackages = trackPackageService.packageRepository.getAll();
+        Map<String, StringBuilder> packageStatusMessages = new HashMap<>();
 
+        log.info("Getting status of all packages");
         // get their status and compare with the existing status
         for (Package pkg: allPackages) {
-            log.info("Getting status of " + pkg);
             String currStatus = pkg.getStatus();
             trackPackageService.getPackageLatestStatus(pkg);
             String latestStatus = pkg.getStatus();
@@ -32,18 +48,34 @@ public class GetPackageStatusTask extends TimerTask {
             // current status could be null
             if (Objects.equals(currStatus, latestStatus)) continue;
 
-            // TODO: deciding on what information to show the user
-            sendMessage(pkg.getUserId(), String.format("The latest status for your package %s is %s", pkg.getName(), pkg.getStatus()));
+            // Display package's name (if not set, display tracking number)
+            // and the latest status
+            String packageIdentifier = Objects.equals(pkg.getName(), null) ? pkg.getTrackingNumber() : pkg.getName();
+            String statusMessage = String.format("The latest status for your package %s is %s", packageIdentifier, pkg.getStatus());
+            
+            packageStatusMessages.putIfAbsent(pkg.getUserId(), new StringBuilder());
+            packageStatusMessages.get(pkg.getUserId()).append(statusMessage + "\n");
+        }
+
+        log.info("Sending messages of package status updates to users");
+        for (Entry<String, StringBuilder> entry: packageStatusMessages.entrySet()) {
+            sendMessage(entry.getKey(), entry.getValue().toString());
         }
     }
 
-    public void sendMessage(String userId, String content) {
-        log.info("Sending message to user "+ userId +" : " + content);
+    /**
+     * Send the latest status of all packages for a user in discord private channel
+     * 
+     * @param userId - discord user id
+     * @param content - a long message string which contains the updates
+     */
+    private void sendMessage(String userId, String content) {
+        
         jda.retrieveUserById(userId).queue(user -> {
                 user.openPrivateChannel()
                     .flatMap(channel -> channel.sendMessage(content))
                     .queue();
             });
-        
+        log.info("Package status updates have been sent to discord user " + userId);
     }
 }
