0d8feb3c16f272e2845f0c70db59bc7dc2005573
boweill <122703192+boweill@users.noreply.github.com>
2023-04-18 03:53:13 +0000

    deleted tracing code

--- src/main/java/edu/northeastern/cs5500/starterbot/Bot.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/Bot.java
@@ -4,16 +4,13 @@ import dagger.Component;
 import edu.northeastern.cs5500.starterbot.command.CommandModule;
 import edu.northeastern.cs5500.starterbot.listener.MessageListener;
 import edu.northeastern.cs5500.starterbot.repository.RepositoryModule;
-import edu.northeastern.cs5500.starterbot.service.OpenTelemetryService;
 import edu.northeastern.cs5500.starterbot.service.ServiceModule;
-import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.context.Scope;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
-@Component(modules = {CommandModule.class, RepositoryModule.class})
+@Component(modules = {CommandModule.class, RepositoryModule.class, ServiceModule.class})
 @Singleton
 interface BotComponent {
     public Bot bot();
@@ -25,7 +22,6 @@ public class Bot {
     Bot() {}
 
     @Inject MessageListener messageListener;
-    @Inject OpenTelemetryService openTelemetryService;
     @Inject JDA jda;
 
     static String getBotToken() {
@@ -33,23 +29,8 @@ public class Bot {
     }
 
     void start() {
-        var span = openTelemetryService.span("updateCommands", SpanKind.PRODUCER);
-        try (Scope scope = span.makeCurrent()) {
-            jda.addEventListener(messageListener);
-            CommandListUpdateAction commands = jda.updateCommands();
-            commands.addCommands(messageListener.allCommandData());
-            commands.queue();
-        } catch (Exception e) {
-            log.error("Unable to add message listeners", e);
-            span.recordException(e);
-        } finally {
-            span.end();
-        }
-        @SuppressWarnings("null")
-        @Nonnull
-        Collection<GatewayIntent> intents = EnumSet.noneOf(GatewayIntent.class);
-        JDA jda = JDABuilder.createLight(token, intents).addEventListeners(messageListener).build();
 
+        jda.addEventListener(messageListener);
         CommandListUpdateAction commands = jda.updateCommands();
         commands.addCommands(messageListener.allCommandData());
         commands.queue();
--- src/main/java/edu/northeastern/cs5500/starterbot/service/ServiceModule.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/service/ServiceModule.java
@@ -3,8 +3,6 @@ package edu.northeastern.cs5500.starterbot.service;
 import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
-import io.opentelemetry.api.trace.SpanKind;
-import io.opentelemetry.context.Scope;
 import java.util.Collection;
 import java.util.EnumSet;
 import javax.annotation.Nonnull;
@@ -17,8 +15,6 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 @Module
 @Slf4j
 public abstract class ServiceModule { // NOSONAR
-    @Binds
-    abstract OpenTelemetry bindOpenTelemetry(OpenTelemetryService service);
 
     static String getBotToken() {
         return new ProcessBuilder().environment().get("BOT_TOKEN");
@@ -26,25 +22,15 @@ public abstract class ServiceModule { // NOSONAR
 
     @Provides
     @Singleton
-    static JDA provideJDA(OpenTelemetryService openTelemetryService) {
-        var span = openTelemetryService.span("start", SpanKind.PRODUCER);
-        try (Scope scope = span.makeCurrent()) {
-            String token = getBotToken();
-            if (token == null) {
-                throw new IllegalArgumentException(
-                        "The BOT_TOKEN environment variable is not defined.");
-            }
-            @SuppressWarnings("null")
-            @Nonnull
-            Collection<GatewayIntent> intents = EnumSet.noneOf(GatewayIntent.class);
-            return JDABuilder.createLight(token, intents).build();
-
-        } catch (Exception e) {
-            log.error("Unable to start the bot", e);
-            span.recordException(e);
-        } finally {
-            span.end();
+    static JDA provideJDA() {
+        String token = getBotToken();
+        if (token == null) {
+            throw new IllegalArgumentException(
+                    "The BOT_TOKEN environment variable is not defined.");
         }
-        return null;
+        @SuppressWarnings("null")
+        @Nonnull
+        Collection<GatewayIntent> intents = EnumSet.noneOf(GatewayIntent.class);
+        return JDABuilder.createLight(token, intents).build();
     }
 }
