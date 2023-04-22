b7295305d9ffd7dde520cf37fd4babc00a6ea2a5
Cece <45926850+SIXIN-LI@users.noreply.github.com>
2023-04-22 07:17:10 +0000

    Clean commented/unused code

--- src/main/java/edu/northeastern/cs5500/starterbot/service/PackageSchedulingService.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/service/PackageSchedulingService.java
@@ -3,27 +3,24 @@ package edu.northeastern.cs5500.starterbot.service;
 import java.util.Timer;
 import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
 
+@Slf4j
 /** PackageSchedulingService is in charge of schedule tasks to send package updates to users */
-public class PackageSchedulingService {
+public class PackageSchedulingService implements Service{
 
     // Delay in milliseconds that allows the task to be initialized after the bot is fully started
     public static final long START_DELAY = TimeUnit.MINUTES.toMillis(1);
 
-    JDA jda;
-    TrackPackageService trackPackageService;
+    @Inject JDA jda;
+    @Inject TrackPackageService trackPackageService;
 
     /**
-     * Public constructor
-     *
-     * @param jda - represents a connection to discord
-     * @param trackPackageService
+     * Public constructor for injection 
      */
     @Inject
-    public PackageSchedulingService(JDA jda, TrackPackageService trackPackageService) {
-        this.jda = jda;
-        this.trackPackageService = trackPackageService;
+    public PackageSchedulingService() {
     }
 
     /**
@@ -38,5 +35,10 @@ public class PackageSchedulingService {
                 new GetPackageStatusTask(jda, trackPackageService),
                 START_DELAY,
                 dayIntervalInMilliseconds);
+    }
+
+    @Override
+    public void register() {
+        log.info("Registering PackageSchedulingService");
     }
 }
